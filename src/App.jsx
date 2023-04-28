9c8b29683fa667df3fa1b3406c6b3a24e0d7b6f0
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2023-04-28 09:49:27 +0200

    Fix Import Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import React from "react"
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
+import "./App.css"
+
 
 function App() {    
     return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
+     
+        <div className="App">
+          
+        <Router>
+           <Header/>
+        <Routes>
+        
+        <Route  path="/" element={<Photos />} />          
+        <Route path="/cart" element={ <Cart />}/>
+                   
+        
+    
+        </Routes>
+        </Router>
         </div>
+        
     )
 }
 
